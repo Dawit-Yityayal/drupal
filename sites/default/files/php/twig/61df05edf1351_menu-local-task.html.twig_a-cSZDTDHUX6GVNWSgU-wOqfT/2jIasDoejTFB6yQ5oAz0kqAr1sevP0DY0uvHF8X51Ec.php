bc8c8e339ade40e10f90df78b4498a734b1646c3
dawit yitayal <it.dawit.yitayal@gmail.com>
2022-01-28 00:40:18 -0800

    them

---
+++ sites/default/files/php/twig/61df05edf1351_menu-local-task.html.twig_a-cSZDTDHUX6GVNWSgU-wOqfT/2jIasDoejTFB6yQ5oAz0kqAr1sevP0DY0uvHF8X51Ec.php
@@ -0,0 +1,94 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/creative_responsive_theme/templates/navigation/menu-local-task.html.twig */
+class __TwigTemplate_9c27f906fd6527100b741f78d0652e5f9c0c30c99abb55bd89c48f4f9165da12 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 17
+        echo "<li";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ((($context["is_active"] ?? null)) ? ("is-active") : (""))], "method", false, false, true, 17), 17, $this->source), "html", null, true);
+        echo ">";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["link"] ?? null), 17, $this->source), "html", null, true);
+        echo "</li>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/creative_responsive_theme/templates/navigation/menu-local-task.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 17,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/creative_responsive_theme/templates/navigation/menu-local-task.html.twig", "C:\\xampp\\htdocs\\instant gaming\\themes\\creative_responsive_theme\\templates\\navigation\\menu-local-task.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 17);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
